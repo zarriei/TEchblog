ccbdea722fa62424a6221975b4bcf27a2e00d7b1
zarriei <75182441+zarriei@users.noreply.github.com>
2021-05-25 21:41:04 -0400

    Updates

---
+++ precache-manifest.1b5db86f4819d088920251c8204d318c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae79a5cf6aa3f7b6d81f",
+    "url": "/static/css/main.f0366281.chunk.css"
+  },
+  {
+    "revision": "ae79a5cf6aa3f7b6d81f",
+    "url": "/static/js/main.ae79a5cf.chunk.js"
+  },
+  {
+    "revision": "8cd6ab85bf999600f5b1",
+    "url": "/static/css/1.e63f9eaa.chunk.css"
+  },
+  {
+    "revision": "8cd6ab85bf999600f5b1",
+    "url": "/static/js/1.8cd6ab85.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "705ad06625f0f711f29cceb7e4f57a84",
+    "url": "/index.html"
+  }
+];
